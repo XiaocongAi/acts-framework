8b17953af4a157ec001402ad8ab6f74e1193f027
xai <xiaocong.ai@cern.ch>
2020-02-16 03:12:23 -0800

    Decorate mis-calibration to the GenericDetector

--- Detectors/GenericDetector/src/GenericDetector.cpp
+++ Detectors/GenericDetector/src/GenericDetector.cpp
@@ -9,18 +9,36 @@
 #include "ACTFW/GenericDetector/GenericDetector.hpp"
 
 #include <boost/program_options.hpp>
+#include "ACTFW/Framework/CalibrationDecorator.hpp"
 #include "ACTFW/Framework/IContextDecorator.hpp"
 #include "ACTFW/GenericDetector/BuildGenericDetector.hpp"
 #include "ACTFW/GenericDetector/GenericDetectorElement.hpp"
 #include "ACTFW/GenericDetector/GenericDetectorOptions.hpp"
 #include "Acts/Geometry/TrackingGeometry.hpp"
 #include "Acts/Utilities/Logger.hpp"
+#include "Acts/Utilities/ParameterDefinitions.hpp"
 
 void
 GenericDetector::addOptions(
     boost::program_options::options_description& opt) const
 {
   FW::Options::addGenericGeometryOptions(opt);
+  opt.add_options()(
+      "calib-seed",
+      boost::program_options::value<size_t>()->default_value(1324354657),
+      "Seed for the decorator random numbers.")(
+      "calib-iovsize",
+      boost::program_options::value<size_t>()->default_value(100),
+      "Size of a valid IOV.")(
+      "calib-flushsize",
+      boost::program_options::value<size_t>()->default_value(200),
+      "Span until garbage collection is active.")(
+      "miscalib-sigma",
+      boost::program_options::value<double>()->default_value(10.),
+      "Sigma of the measurement miscalibration in [um]")(
+      "calib-loglevel",
+      boost::program_options::value<size_t>()->default_value(3),
+      "Output log level of the calibration decorator.");
 }
 
 auto
@@ -54,7 +72,40 @@ GenericDetector::finalize(
                                                     surfaceLogLevel,
                                                     layerLogLevel,
                                                     volumeLogLevel);
-  ContextDecorators gContextDeocrators = {};
+  Acts::Logging::Level decoratorLogLevel
+      = Acts::Logging::Level(vm["calib-loglevel"].template as<size_t>());
+
+  // Let's create a reandom number service
+  FW::RandomNumbers::Config randomNumberConfig;
+  randomNumberConfig.seed = vm["calib-seed"].template as<size_t>();
+  auto randomNumberSvc
+      = std::make_shared<FW::RandomNumbers>(randomNumberConfig);
+
+  // The miscalibration generators
+  std::map<const Acts::Surface*, FW::Contextual::CalibDataGenFunction>
+      misCalibGenerator;
+  for (const auto& layer : detectorStore) {
+    for (const auto& det : layer) {
+      // TODO: consider the dimension of measurements
+      FW::Contextual::MisCalibrationGenerator miscalibGen{
+          {{Acts::ParDef::eLOC_0, vm["miscalib-sigma"].template as<double>()},
+           {Acts::ParDef::eLOC_1, vm["miscalib-sigma"].template as<double>()}}};
+      misCalibGenerator.emplace(&det->surface(), std::move(miscalibGen));
+    }
+  }
+
+  // Calibration decorator service
+  FW::Contextual::CalibrationDecorator::Config calibDecoConfig;
+  calibDecoConfig.calibrationDataGenerator = std::move(misCalibGenerator);
+  calibDecoConfig.iovSize         = vm["calib-iovsize"].template as<size_t>();
+  calibDecoConfig.randomNumberSvc = randomNumberSvc;
+
+  // Make the context decorators
+  ContextDecorators gContextDeocrators
+      = {std::make_shared<FW::Contextual::CalibrationDecorator>(
+          calibDecoConfig,
+          Acts::getDefaultLogger("CalibrationDecorator", decoratorLogLevel))};
+
   // return the pair of geometry and empty decorators
   return std::make_pair<TrackingGeometryPtr, ContextDecorators>(
       std::move(gGeometry), std::move(gContextDeocrators));
