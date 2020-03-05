ab84f47a0e38caa164eb8665da75b7feeef14b16
Georgiana Mania <georgiana.mania@cern.ch>
2020-03-05 17:23:28 +0100

    implement review changes

--- scripts/testReproducibility.sh
+++ scripts/testReproducibility.sh
@@ -2,32 +2,31 @@
 #
 # This script tests whether the output of a certain ACTS framework example is
 # reproducible between single-threaded and multi-threaded runs. For example,
-# "./testReproducibility.sh GenericFatras" will run the ACTFWGenericFatrasExample
-# in single-threaded and multi-threaded mode and check whether the output is
-# the same aside from threading-induced event reordering.
+# "./testReproducibility.sh ACTFWGenericFatrasExample" will run the
+# ACTFWGenericFatrasExample in single-threaded and multi-threaded mode and
+# check whether the output is the same aside from threading-induced event reordering.
 #
 set -uo pipefail
 
 # Check whether the user did specify the name of the example to be run
 ARGC=$#
-if [[ $ARGC -lt 3 ]]; then
+if [[ $ARGC -lt 2 ]]; then
   echo ""
-  echo " Usage: "$0" <example> <inputParameter> <inputValue> <output1> [<output2> ...]"
+  echo " Usage: "$0" <example> <flags> <output1> [<output2> ...]"
   echo ""
-  echo " <inputParameter> is the name of the input parameter (e.g. events)"
-  echo " <inputValue> is the value for the given input parameter (e.g. 5)"
-  echo " <example> is the example name (which is the executable name without the leading 'ACTFW' and the trailing 'Example')"
+  echo " <example> is the executable name (e.g. ACTFWGenericFatrasExample)"
+  echo " <flags> is a string containing CLI flags (e.g. \"-n 5\")"
   echo " <outputN> is the output name (which is the output file name without the trailing '.root')"
   echo ""
   exit 42
 fi
 
 # Compute the name of the example executable
-executable="ACTFW$1Example --$2=$3 --output-root true"
+executable="$1 $2 --output-root true"
 echo ${executable}
 
 # Compute the output file names
-for ((i = 4; i <= $ARGC; i++)); do
+for ((i = 3; i <= $ARGC; i++)); do
   eval output=\$${i}.root
   eval outputs[$i]=$output
 done
